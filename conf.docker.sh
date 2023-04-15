8544e913fb5252e7eb81acd64818d08354a98ba9
randomguy-on-internet <ali.56341309@gmail.com>
2023-04-15 20:01:45 +0330

    Update conf.docker.sh

--- conf.docker.sh
+++ conf.docker.sh
@@ -7,7 +7,7 @@ pub=$(echo "$keys" | awk '/Public key:/ {print $3}')
 serverIp=$(curl -s ifconfig.me)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
-url="vless://$uuid@$serverIp:443?path=%2F&security=reality&encryption=none&flow=xtls-rprx-vision&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
+url="vless://$uuid@$serverIp:17515?path=%2F&security=reality&encryption=none&flow=xtls-rprx-vision&pbk=$pub&fp=chrome&type=http&sni=yahoo.com&sid=$shortId#IRVLESS-REALITY-04"
 
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
